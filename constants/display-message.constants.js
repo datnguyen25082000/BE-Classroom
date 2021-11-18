4de0e7577d677ce3252b92fa788c17e217bbd8e6
Nguyen Van Duc <nguyenduc21022k@gmail.com>
2021-11-18 20:14:51 +0700

    Create constant

---
+++ constants/display-message.constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+  USERNAME_NOT_EXIST: "USERNAME_NOT_EXIST",
+  INCORRECT_PASSWORD: "INCORRECT_PASSWORD",
+  USERNAME_ALREADY_EXISTS: "Tên đăng nhập đã tồn tại",
+  REGISTER_USER_SUCCESSFULLY: "Đăng ký tài khoản thành công",
+  REGISTER_USER_FAILED: "Đăng ký thất bại",
+};
