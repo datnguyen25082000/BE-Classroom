41d116d28bd0171cf342e45dff20ad94488aa7c2
Nguyen Van Duc <nguyenduc21022k@gmail.com>
2021-11-21 16:54:55 +0700

    Add course-join model

---
+++ model/course-join.model.js
@@ -0,0 +1,39 @@
+const db = require("../utils/db");
+
+const TBL_COURSE_JOIN = "coursejoin";
+
+module.exports = {
+  allByUser(userId) {
+    return db.load(
+      `select * from ${TBL_COURSE_JOIN} where user_id = ${userId}`
+    );
+  },
+
+  allByCourse(courseId) {
+    return db.load(
+      `select * from ${TBL_COURSE_JOIN} where course_id = ${courseId}`
+    );
+  },
+
+  async single(userId, courseId) {
+    const rows = await db.load(
+      `select * from ${TBL_COURSE_JOIN} where course_id = ${courseId} and user_id = ${userId}`
+    );
+
+    return rows.length ? rows[0] : null;
+  },
+
+  add(entity) {
+    return db.add(entity, TBL_COURSE_JOIN);
+  },
+
+  del(entity) {
+    const condition = { course_id: entity.course_id, user_id: entity.user_id };
+    return db.del(condition, TBL_COURSE_JOIN);
+  },
+
+  patch(entity) {
+    const condition = { course_id: entity.course_id, user_id: entity.user_id };
+    return db.patch(entity, condition, TBL_COURSE_JOIN);
+  },
+};
