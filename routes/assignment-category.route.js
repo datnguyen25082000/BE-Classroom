33606196d8bc886ce2e76a7ca09a9ee7381e13d0
Nguyen Van Duc <nguyenduc21022k@gmail.com>
2021-12-18 15:30:43 +0700

    api to finalize an assignment category

--- routes/assignment-category.route.js
+++ routes/assignment-category.route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const assignmentCategoryController = require('../controllers/assignment-category.controller')
+const assignmentCategoryController = require("../controllers/assignment-category.controller");
 
 /* GET home page. */
-router.get('/get-all-by-course', assignmentCategoryController.getAllByCourse)
-router.post('/add', assignmentCategoryController.add)
-router.post('/update', assignmentCategoryController.update)
-router.post('/update-position', assignmentCategoryController.updatePosition)
-router.post('/delete', assignmentCategoryController.delete)
+router.get("/get-all-by-course", assignmentCategoryController.getAllByCourse);
+router.post("/add", assignmentCategoryController.add);
+router.post("/update", assignmentCategoryController.update);
+router.post("/update-position", assignmentCategoryController.updatePosition);
+router.post("/delete", assignmentCategoryController.delete);
+router.post("/finalize", assignmentCategoryController.finalize);
 
 module.exports = router;
